a00faac6bc60f851f70361dca49942575aa7fd64
hotwellkz <hotwell.kz@gmail.com>
2024-11-20 00:43:29 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigation } from './components/Navigation';
-import { HeroSection } from './components/HeroSection';
-import { FeaturesSection } from './components/FeaturesSection';
-import { ComparisonSection } from './components/ComparisonSection';
-import { HowToSection } from './components/HowToSection';
-import { ScriptSection } from './components/ScriptSection';
-import { AnalysisSection } from './components/AnalysisSection';
-import { ReelsLibrary } from './components/ReelsLibrary';
-import { PricingSection } from './components/PricingSection';
-import { FAQSection } from './components/FAQSection';
-import { CTASection } from './components/CTASection';
-import { Footer } from './components/Footer';
-import { SearchSection } from './components/SearchSection';
-import { RealTimeDataSection } from './components/RealTimeDataSection';
-import { AuthPage } from './components/AuthPage';
-import { SignUpPage } from './components/SignUpPage';
-import { Dashboard } from './components/Dashboard';
-import { BillingPage } from './components/BillingPage';
-import { PrivacyPolicy } from './components/PrivacyPolicy';
-import { CookiesPolicy } from './components/CookiesPolicy';
-import { TermsConditions } from './components/TermsConditions';
-import SubscribersPage from './components/SubscribersPage';
-import { ProfileEditPage } from './components/ProfileEditPage';
-import { PartnersPage } from './components/PartnersPage';
-import { ChatWidget } from './components/ChatWidget';
-import { useAuth } from './hooks/useAuth';
-
-function HomePage() {
-  return (
-    <>
-      <Navigation />
-      <HeroSection />
-      <FeaturesSection />
-      <ComparisonSection />
-      <HowToSection />
-      <ScriptSection />
-      <AnalysisSection />
-      <SearchSection />
-      <RealTimeDataSection />
-      <ReelsLibrary />
-      <PricingSection />
-      <FAQSection />
-      <CTASection />
-      <Footer />
-    </>
-  );
-}
+import { HelmetProvider } from 'react-helmet-async';
+// ... остальные импорты
 
 function App() {
   const { user } = useAuth();
 
   return (
-    <Router>
-      <div className="min-h-screen bg-black text-white">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<AuthPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/billing" element={<BillingPage />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="/cookies" element={<CookiesPolicy />} />
-          <Route path="/terms" element={<TermsConditions />} />
-          <Route path="/subscribers" element={<SubscribersPage />} />
-          <Route path="/profile/edit" element={<ProfileEditPage />} />
-          <Route path="/partners" element={<PartnersPage />} />
-        </Routes>
-        {user && <ChatWidget />}
-      </div>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <div className="min-h-screen bg-black text-white">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* ... остальные роуты */}
+          </Routes>
+          {user && <ChatWidget />}
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 }
-
-export default App;

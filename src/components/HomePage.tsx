1332848a5b3143cb169d72a81fade451e49ba9cb
hotwellkz <hotwell.kz@gmail.com>
2024-11-20 03:12:52 +0600

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,18 +2,13 @@ import React from 'react';
 import { MetaTags } from './SEO/MetaTags';
 import { Navigation } from './Navigation';
 import { HeroSection } from './HeroSection';
-import { FeaturesSection } from './FeaturesSection';
-import { ComparisonSection } from './ComparisonSection';
-import { HowToSection } from './HowToSection';
-import { ScriptSection } from './ScriptSection';
-import { AnalysisSection } from './AnalysisSection';
-import { ReelsLibrary } from './ReelsLibrary';
-import { PricingSection } from './PricingSection';
-import { FAQSection } from './FAQSection';
+import { ScriptFeatures } from './ScriptFeatures';
+import { ScriptExamples } from './ScriptExamples';
+import { ScriptBenefits } from './ScriptBenefits';
+import { ScriptFAQ } from './ScriptFAQ';
 import { CTASection } from './CTASection';
+import { RelatedLinks } from './RelatedLinks';
 import { Footer } from './Footer';
-import { SearchSection } from './SearchSection';
-import { RealTimeDataSection } from './RealTimeDataSection';
 
 export function HomePage() {
   return (
@@ -22,17 +17,12 @@ export function HomePage() {
       <Navigation />
       <main>
         <HeroSection />
-        <FeaturesSection />
-        <ComparisonSection />
-        <HowToSection />
-        <ScriptSection />
-        <AnalysisSection />
-        <SearchSection />
-        <RealTimeDataSection />
-        <ReelsLibrary />
-        <PricingSection />
-        <FAQSection />
+        <ScriptFeatures />
+        <ScriptExamples />
+        <ScriptBenefits />
+        <ScriptFAQ />
         <CTASection />
+        <RelatedLinks />
       </main>
       <Footer />
     </>
